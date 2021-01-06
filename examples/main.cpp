c8656fce1db68d1c311d9be1cbdaef4b7cc239e6
Eren <erencemayez@gmail.com>
2021-01-06 20:23:23 +0300

    Add files via upload

---
+++ examples/main.cpp
@@ -0,0 +1,68 @@
+#define STASSID "SSID"
+#define STAPSK  "PASS"
+
+#define APSSID  "TESTEST"
+
+#include <ESP8266WiFi.h>
+#include <IPAddress.h>
+#include <dhcpserver.h>
+#include <lwip/napt.h>
+
+extern "C"{
+  #include "netfilter.h"
+}
+
+/*============  SOFTAP ===========*/
+IPAddress SOFTAP_LOCAL_IP(192,168,2,1);
+#define SOFTAP_GATEWAY_IP SOFTAP_LOCAL_IP
+IPAddress SOFTAP_SUBNET_IP(255,255,255,0);
+
+struct netfilter n; // <-- GLOBAL
+
+err_t test_link_output(netif *netif, pbuf *p){
+
+  Serial.println("Function executed");
+  return netfilter_out(n, netif, p);
+
+}
+
+void setup() {
+
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(STASSID, STAPSK);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+
+  WiFi.softAPConfig(SOFTAP_LOCAL_IP, SOFTAP_GATEWAY_IP, SOFTAP_SUBNET_IP);
+
+  #ifdef APPSK
+  WiFi.softAP(APSSID, APPSK);
+  #else
+  WiFi.softAP(APSSID);
+  #endif
+
+  /* NAPT */
+  Serial.println("Enable nat");
+
+  ip_napt_init(IP_NAPT_MAX, IP_PORTMAP_MAX);
+  ip_napt_enable_no(1, 1); // enable napt for ap interface
+
+  dhcps_set_dns(0, WiFi.dnsIP(0));
+  dhcps_set_dns(1, WiFi.dnsIP(1));
+
+
+  n = netfilter_new(test_link_output);
+  netfilter_start(&n, NETIF_AP);
+
+  Serial.printf("\n%p : %p function pointers old vs new linkoutput", n.old_output_fn, n.new_output_fn);
+
+}
+
+void loop() {
+  delay(500);
+}
---
+++ netfilter.h
@@ -0,0 +1,33 @@
+#ifndef NETFILTER_H
+#define NETFILTER_H
+
+#include "lwip/netif.h"
+
+typedef enum NetifType {
+
+    NETIF_STA = 0,
+    NETIF_AP = 1
+
+} NetifType_t;
+
+typedef struct netfilter{
+
+  netif_linkoutput_fn new_output_fn;
+  netif_linkoutput_fn old_output_fn;
+
+} netfilter_t;
+
+/*
+  
+  netif_input_fn input; <-- functin called when phisycal layer recives ethernet frame
+  netif_output_fn output; <-- function called when ip packet want to be sent
+  netif_linkoutput_fn linkoutput; <-- calls phisycal driver to output ethernet packets
+
+*/
+
+struct netfilter netfilter_new(netif_linkoutput_fn func);
+int netfilter_start(struct netfilter *n, NetifType_t type);
+int netfilter_stop (struct netfilter n, NetifType_t type);
+err_t netfilter_out(struct netfilter n, struct netif* netif, struct pbuf *p);
+
+#endif // NETFILTER_H
